f39252af2dbbabeaa70ea6d7772bf102ad5013ce
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-09 19:31:30 -0400

    inital comit

---
+++ src/main/java/org/rezatron/rezatron_config_server/RezatronConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.rezatron.rezatron_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezatronConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezatronConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rezatron/rezatron_config_server/RezatronConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rezatron.rezatron_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezatronConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
