d8801e06d4d6ce7da6974514564b8eea2de1f6c7
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-12 22:41:11 -0400

    working for git our vault but not both

--- src/main/java/org/rezatron/rezatron_config_server/RezatronConfigServerApplication.java
+++ src/main/java/org/rezatron/rezatron_config_server/RezatronConfigServerApplication.java
@@ -1,13 +1,37 @@
 package org.rezatron.rezatron_config_server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableConfigServer
 public class RezatronConfigServerApplication {
+
+	    @Autowired
+    private Environment environment;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RezatronConfigServerApplication.class, args);
 	}
 
+	    @PostConstruct
+    public void checkVaultUri() {
+        String vaultUri = environment.getProperty("spring.cloud.vault.uri");
+        System.out.println("Configured Vault URI: " + vaultUri);
+    }
+
+	@PostConstruct
+public void checkActiveProfiles() {
+    String[] activeProfiles = environment.getActiveProfiles();
+    System.out.println("Active profiles: " + String.join(", ", activeProfiles));
 }
+
+}
+
+
