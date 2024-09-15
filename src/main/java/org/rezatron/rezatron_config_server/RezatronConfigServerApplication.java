f12df16f56e20c86bb606a7c1f72c6675192112b
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-15 12:08:48 -0400

    update logback and add password for security

--- src/main/java/org/rezatron/rezatron_config_server/RezatronConfigServerApplication.java
+++ src/main/java/org/rezatron/rezatron_config_server/RezatronConfigServerApplication.java
@@ -2,36 +2,30 @@ package org.rezatron.rezatron_config_server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.core.env.Environment;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableConfigServer
 public class RezatronConfigServerApplication {
 
-	    @Autowired
+    @Autowired
     private Environment environment;
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(RezatronConfigServerApplication.class, args);
-	}
-
-	    @PostConstruct
-    public void checkVaultUri() {
-        String vaultUri = environment.getProperty("spring.cloud.vault.uri");
-        System.out.println("Configured Vault URI: " + vaultUri);
+    public static void main(String[] args) {
+        SpringApplication.run(RezatronConfigServerApplication.class, args);
     }
 
-	@PostConstruct
-public void checkActiveProfiles() {
-    String[] activeProfiles = environment.getActiveProfiles();
-    System.out.println("Active profiles: " + String.join(", ", activeProfiles));
-}
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "rezatron-config-server");
+    }
 
 }
-
-
